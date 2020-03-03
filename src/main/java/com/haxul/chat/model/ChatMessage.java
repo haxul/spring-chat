4c957567a127ea6313d10641f9f89477f826c166
haxul <s.s.starodubov@yandex.ru>
2020-03-03 22:20:00 +0400

    add setter and getter for ChatMessage.class

--- src/main/java/com/haxul/chat/model/ChatMessage.java
+++ src/main/java/com/haxul/chat/model/ChatMessage.java
@@ -1,6 +1,10 @@
 package com.haxul.chat.model;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class ChatMessage {
     private MessageType type;
     private String content;
@@ -10,29 +14,5 @@ public class ChatMessage {
         CHAT,
         JOIN,
         LEAVE
-    }
-
-    public MessageType getType() {
-        return type;
-    }
-
-    public void setType(MessageType type) {
-        this.type = type;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
     }
 }
